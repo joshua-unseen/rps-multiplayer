6ef532fe309ce3eab93d90314e8d1c17abc74fd1
joshua-unseen <corpselight@gmail.com>
2019-04-06 08:38:36 -0700

    Fighting with logic.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,15 +9,39 @@ var config = {
 };
 firebase.initializeApp(config);
 
+var base = firebase.database();
+var rootRef = base.ref();
+var connRef = base.ref("/connections");
+var queueRef = base.ref("/queue");
+var playersRef = base.ref("/players");
+var chatRef = base.ref("/chat");
+
+var amConnected = base.ref(".info/connected");
+
+amConnected.on("value", function(snap) {
+  if (snap.val()) {
+    var con = connRef.push(true);
+    con.onDisconnect().remove();
+  }
+  //    The connections object changes each time the page is reloaded.  Will have to store a UUID in cookie or localStorage.
+});
+
+rootRef.on("child_added", function(snap){
+    var queueVal = snap.child("queue").val();
+    console.log(snap);
+    if (snap.child("players").numChildren() < 2) {
+        playersRef.push(queueVal[0]);
+    }
+});
 /* App flow:
-    We'll need a few database nodes, say:
+We'll need a few database nodes, say:
     /players (capped at 2 members), 
-    /waiting, to hold the people connected, say { user: "", timestamp: "" }
+    /waiting, to hold the people connected, say { user: "", timestamp: "", wins: "", losses: "", }
     /chat, to hold { user: "", message: "" } records
 
     as users load the page, get a name and add them to the queue.
     If the queue fills by order of connection, we may not need a timestamp.
-    That will simplify things.
+    That will simplify things: less math before we get to it.
     while /players length is less than 2, push /waiting[0] to /players and remove /waiting[0]
     When a user submits a message, store their name and message in /messages, then display it in the chat box
     Show waiting users in the queue div.
@@ -26,10 +50,25 @@ firebase.initializeApp(config);
 
     Gameplay:
     Wait for one player to choose an option, give the other player 5 (?) seconds to choose theirs.
-    Store both choices, compare them, boot the loser, fill /players again.
+    Store both choices, compare them, boot the loser to the end of /waiting, fill /players again.
+    Modals-- name prompt, R/P/S choice.
+    On load, prompt for name.
 */
 
-
 var game = {
+    playerName: "",
+    player: "",
+    playerObj: {"name": "",
+        "wins": 0,
+        "losses": 0,
+    },
 
+    GetName() {
+        this.playerName = prompt("Enter your name:");
+        console.log(this.playerName);
+        this.playerObj.name = this.playerName;
+        this.player = queueRef.push(this.playerObj);
+        console.log(this.player.key);   // store the key to access the obj later!
+        this.player.onDisconnect().remove();
+    }
 }
